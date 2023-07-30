f03efe919c9c4febb60d18b37c998299e2a30444
Yusuf Afif <afif.yusuf@gmail.com>
2023-07-30 23:51:18 +0700

    bismillah

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
-const express = require('express');
-const mysql = require('mysql');
+const express = require("express")
+const mysql = require("mysql")
 
-const app = express();
+const app = express()
 
-app.get("/", (req, res) => {
-    res.send("OK ROUTE OPEN");
-});
+const db = mysql.createConnection({
+    host: "localhost",
+    database: "belajardb",
+    user: "root",
+    password: "",
+})
+
+db.connect((err) => {
+    if (err) throw err
+    console.log("DB Connected")
+
+    const sql = "SELECT * FROM rpul"
+    db.query(sql, (err, result) => {
+        console.log('hasil database', result)
+    })
+
+    app.get("/", (req, res) => {
+        res.send("OK ROUTE OPEN")
+    })
+})
+
 
 app.listen(8000, () => {
-    console.log("Server ready...");
-});
+    console.log("Server ready...")
+})
