7bc55bd9740da0a684e7ff70964bc4be036dde22
Yusuf Afif <afif.yusuf@gmail.com>
2023-08-02 22:12:56 +0700

    Insert selesai

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const mysql = require("mysql")
+const bodyParser = require("body-parser")
 
 const app = express()
 
+app.use(bodyParser.urlencoded({ extended: true}))
+
 app.set("view engine", "ejs")
 app.set("views", "views")
-
+ 
 const db = mysql.createConnection({
     host: "localhost",
     database: "belajardb",
@@ -16,15 +19,25 @@ const db = mysql.createConnection({
 db.connect((err) => {
     if (err) throw err
     console.log("DB Connected")
-
-    const sql = "SELECT * FROM rpul"
-    db.query(sql, (err, result) => {
-        const users = JSON.parse(JSON.stringify(result))
-        console.log('hasil database', users)
-        app.get("/", (req, res) => {
-            res.render("index", { users: users, title: "TEST"})
+    
+    
+    app.get("/", (req, res) => {
+        const sql = "SELECT * FROM rpul"
+        db.query(sql, (err, result) => {
+            const users = JSON.parse(JSON.stringify(result))
+            res.render("index", { users: users, title: "Belajar CRUD" })
         })
     })
+
+    app.post("/tambah", (req, res) => {
+        const insertSql = `INSERT INTO rpul (no, nama_lengkap, umur, premis) VALUES ('${req.body.no}', '${req.body.nama}', '${req.body.umur}', '${req.body.premis}');`
+        db.query(insertSql, (err, result) => {
+            if (err) throw err
+            res.redirect("/")
+        })
+    })
+
+   
 })
 
 
